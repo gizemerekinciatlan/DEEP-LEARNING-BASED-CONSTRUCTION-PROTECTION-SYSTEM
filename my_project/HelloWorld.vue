703fe8027450c57fbe96bf0069c04a01ef9f843a
gizemerekinciatlan <120285850+gizemerekinciatlan@users.noreply.github.com>
2026-08-08 00:37:54 +0200

    Add files via upload

---
+++ my_project/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2 @click="show_video">123123</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    show_video(){
+      this.$router.push('/about')
+    }
+  }
+}
+
+</script>
+
+<style>
+.map {
+  width: 100%;
+  height: 800px;
+}
+</style>
